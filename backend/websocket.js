406b585a62c4eb8828ac549b9f979a02b5bd05b1
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2026-05-26 10:15:32 +0200

    Updated the server logic

--- backend/websocket.js
+++ backend/websocket.js
@@ -1,9 +1,26 @@
+import { createServer } from 'http'
 import { WebSocketServer } from 'ws'
 
-const wss = new WebSocketServer({ port: 8080 })
+const server = createServer()
+const wss = new WebSocketServer({ server })
 
-wss.on('message', function message(data) {
-  wss.send(data)
+wss.on('connection', (socket) => {
+  console.log('Client connected')
+  socket.on('message', (data) => {
+    const message = data.toString()
+    console.log('Received', message)
+
+    for (const client of wss.client) {
+      if (client.readyState == 1) {
+        client.send(message)
+      }
+    }
+  })
+  socket.on('close', () => {
+    console.log('Client disconnected')
+  })
 })
 
-console.log(`Websocket server is running`)
+server.listen(3000, () => {
+  console.log('Server running')
+})
